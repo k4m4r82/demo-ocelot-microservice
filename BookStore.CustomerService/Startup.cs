71c780cdcf4562fbf9e218e2ca2b17bae5a9fcef
Kamarudin <rudi.krsoftware@gmail.com>
2018-04-05 16:56:42 +0700

    First commit

---
+++ BookStore.CustomerService/Startup.cs
@@ -0,0 +1,100 @@
+﻿/**
+ * Copyright (C) 2018 Kamarudin (http://coding4ever.net/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * The latest version of this file can be found at https://github.com/k4m4r82/demo-ocelot-microservice
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.CustomerService.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.CustomerService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var server = Configuration["DBHOST"] ?? "127.0.0.1";
+            var port = Configuration["DBPORT"] ?? "5432";
+
+            var dbName = "BookStore";
+            var userName = "postgres";
+            var userPassword = "masterkey";
+
+            var connection = string.Format("Server={0};Port={1};User Id={2};Password={3};Database={4}", 
+                server, port, userName, userPassword, dbName);
+
+            //Inject
+            services.AddDbContext<CustomerContext>(options => options.UseNpgsql(connection));
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            /*
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new Info { Title = "Product APIs", Version = "v1" });
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });*/
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Ocelot - API Gateway untuk .NET
+
+Ini adalah contoh implementasi Ocelot yang bisa digunakan sebagai api gateway dalam pengembangan microservice.
+
+Untuk infomasi lebih lanjut tentang Ocelot bisa Anda cek di http://ocelot.readthedocs.io/en/latest/
+
+### Pre-requisites
+
+* Visual Studio 2017 15.3+
+* Docker
